b7842e21350d8c87aa243e3ec59950d2a32b5e62
raeleus <raymond.ray3k@gmail.com>
2021-02-06 09:14:09 -0800

    Added GDX-VFX and underwater effect.

--- core/src/com/ray3k/demonstration/Core.java
+++ core/src/com/ray3k/demonstration/Core.java
@@ -3,35 +3,58 @@ package com.ray3k.demonstration;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.crashinvaders.vfx.VfxManager;
+import com.crashinvaders.vfx.effects.WaterDistortionEffect;
 
 public class Core extends ApplicationAdapter {
     Texture jungleTexture;
     Texture peachTexture;
     SpriteBatch batch;
-	
-	@Override
-	public void create () {
+    VfxManager vfxManager;
+    WaterDistortionEffect waterEffect;
+    
+    @Override
+    public void create() {
+        vfxManager = new VfxManager(Format.RGBA8888);
+        waterEffect = new WaterDistortionEffect(10f, .5f);
+        vfxManager.addEffect(waterEffect);
+        
         jungleTexture = new Texture("jungle.png");
         peachTexture = new Texture("peach.png");
         batch = new SpriteBatch();
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+    }
+    
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        vfxManager.update(Gdx.graphics.getDeltaTime());
+        vfxManager.cleanUpBuffers();
+        vfxManager.beginInputCapture();
         batch.begin();
         batch.draw(jungleTexture, 0, 0);
         batch.draw(peachTexture, Gdx.input.getX() - peachTexture.getWidth() / 2f, Gdx.graphics.getHeight() - Gdx.input.getY() - peachTexture.getHeight() / 2f);
         batch.end();
-	}
-	
-	@Override
-	public void dispose () {
+        vfxManager.endInputCapture();
+        vfxManager.applyEffects();
+        vfxManager.renderToScreen();
+    }
+    
+    @Override
+    public void dispose() {
         jungleTexture.dispose();
+        peachTexture.dispose();
         batch.dispose();
-	}
+        vfxManager.dispose();
+        waterEffect.dispose();
+    }
+    
+    @Override
+    public void resize(int width, int height) {
+        vfxManager.resize(width, height);
+    }
 }
